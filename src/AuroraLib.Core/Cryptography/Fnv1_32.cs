1826527ab87124e008a610b5d0d71892917df390
Venomalia <Venomalia@mailbox.org>
2023-09-16 01:12:55 +0200

    add Fnv1 32 & 64

---
+++ src/AuroraLib.Core/Cryptography/Fnv1_32.cs
@@ -0,0 +1,58 @@
+﻿using AuroraLib.Core.Interfaces;
+using System.Runtime.CompilerServices;
+
+namespace AuroraLib.Core.Cryptography
+{
+    /// <summary>
+    /// 32-bit FNV-1 algorithms.
+    /// </summary>
+    public sealed class Fnv1_32 : IHash<uint>
+    {
+        private const uint Prime = 0x01000193;
+        private const uint Initial = 0x811c9dc5;
+        private readonly bool A;
+
+        /// <inheritdoc />
+        public uint Value { get; private set; }
+
+        public Fnv1_32()
+            => Value = Initial;
+
+        public Fnv1_32(bool a) : this()
+            => A = a;
+
+        /// <inheritdoc />
+        public void Compute(ReadOnlySpan<byte> input)
+        {
+            if (A)
+            {
+                for (int i = 0; i < input.Length; i++)
+                {
+                    Value = (Value ^ input[i]) * Prime;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < input.Length; i++)
+                {
+                    Value = (Value * Prime) ^ input[i];
+                }
+            }
+        }
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public byte[] GetBytes()
+            => BitConverterX.GetBytes(Value);
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Reset()
+            => Value = Initial;
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void SetSeed(uint seed = 1)
+            => Value = seed;
+    }
+}
---
+++ src/AuroraLib.Core/Cryptography/Fnv1_64.cs
@@ -0,0 +1,58 @@
+﻿using AuroraLib.Core.Interfaces;
+using System.Runtime.CompilerServices;
+
+namespace AuroraLib.Core.Cryptography
+{
+    /// <summary>
+    /// 64-bit FNV-1 algorithms.
+    /// </summary>
+    public sealed class Fnv1_64 : IHash<ulong>
+    {
+        private const ulong Prime = 0x00000100000001B3;
+        private const ulong Initial = 0xcbf29ce484222325;
+        private readonly bool A;
+
+        /// <inheritdoc />
+        public ulong Value { get; private set; }
+
+        public Fnv1_64()
+            => Value = Initial;
+
+        public Fnv1_64(bool a) : this()
+            => A = a;
+
+        /// <inheritdoc />
+        public void Compute(ReadOnlySpan<byte> input)
+        {
+            if (A)
+            {
+                for (int i = 0; i < input.Length; i++)
+                {
+                    Value = (Value ^ input[i]) * Prime;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < input.Length; i++)
+                {
+                    Value = (Value * Prime) ^ input[i];
+                }
+            }
+        }
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public byte[] GetBytes()
+            => BitConverterX.GetBytes(Value);
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Reset()
+            => Value = Initial;
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void SetSeed(ulong seed = 1)
+            => Value = seed;
+    }
+}
