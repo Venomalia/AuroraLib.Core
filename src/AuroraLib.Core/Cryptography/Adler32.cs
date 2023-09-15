174d3ba432410cdce994d2b478211cec9a1f499b
Venomalia <Venomalia@mailbox.org>
2023-09-15 22:28:13 +0200

    add Adler32 & Adler64

---
+++ src/AuroraLib.Core/Cryptography/Adler32.cs
@@ -0,0 +1,57 @@
+﻿using AuroraLib.Core.Interfaces;
+using System.Runtime.CompilerServices;
+
+namespace AuroraLib.Core.Cryptography
+{
+    /// <summary>
+    /// 32-bit Adler implementation.
+    /// </summary>
+    public sealed class Adler32 : IHash<uint>
+    {
+        private const uint Prime = 65521;
+
+        public uint Value { get; private set; }
+
+        public Adler32()
+            => Reset();
+
+        /// <inheritdoc />
+        public void Compute(ReadOnlySpan<byte> input)
+        {
+            uint s1 = Value & 0xFFFF;
+            uint s2 = Value >> 16;
+            var count = input.Length;
+            var offset = 0;
+            while (count > 0)
+            {
+                int n = Math.Min(3800, count);
+                count -= n;
+
+                while (--n >= 0)
+                {
+                    s1 += input[offset++];
+                    s2 += s1;
+                }
+
+                s1 %= Prime;
+                s2 %= Prime;
+            }
+            Value = (s2 << 16) | s1;
+        }
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public byte[] GetBytes()
+            => BitConverterX.GetBytes(Value);
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Reset()
+            => Value = 1;
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void SetSeed(uint seed = 1)
+            => Value = seed;
+    }
+}
---
+++ src/AuroraLib.Core/Cryptography/Adler64.cs
@@ -0,0 +1,57 @@
+﻿using AuroraLib.Core.Interfaces;
+using System.Runtime.CompilerServices;
+
+namespace AuroraLib.Core.Cryptography
+{
+    /// <summary>
+    /// 64-bit Adler implementation.
+    /// </summary>
+    public class Adler64 : IHash<ulong>
+    {
+        private const ulong Prime = 4294967291;
+
+        public ulong Value { get; private set; }
+
+        public Adler64()
+            => Reset();
+
+        /// <inheritdoc />
+        public void Compute(ReadOnlySpan<byte> input)
+        {
+            ulong s1 = Value & 0xFFFFFFFF;
+            ulong s2 = Value >> 32;
+            var count = input.Length;
+            var offset = 0;
+            while (count > 0)
+            {
+                int n = Math.Min(65536, count);
+                count -= n;
+
+                while (--n >= 0)
+                {
+                    s1 += input[offset++];
+                    s2 += s1;
+                }
+
+                s1 %= Prime;
+                s2 %= Prime;
+            }
+            Value = (s2 << 32) | s1;
+        }
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public byte[] GetBytes()
+            => BitConverterX.GetBytes(Value);
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Reset()
+            => Value = 1;
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void SetSeed(ulong seed)
+            => Value = seed;
+    }
+}
