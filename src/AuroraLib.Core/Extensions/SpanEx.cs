0ad22b3c0a89ad45f22cc9081853c77c654262ce
Venomalia <87765258+Venomalia@users.noreply.github.com>
2023-10-24 23:23:48 +0200

    add SpanEx MaxMatch

--- src/AuroraLib.Core/Extensions/SpanEx.cs
+++ src/AuroraLib.Core/Extensions/SpanEx.cs
@@ -16,19 +16,19 @@ namespace AuroraLib.Core.Extensions
         /// <returns>true if the sequence is found within the larger sequence; otherwise, false.</returns>
         [DebuggerStepThrough]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool SequenceContains<T>(this ReadOnlySpan<T> values, ReadOnlySpan<T> sequence)
+        public static bool SequenceContains<T>(this ReadOnlySpan<T> values, ReadOnlySpan<T> sequence) where T : IEquatable<T>
             => SequenceSearch(values, sequence) != -1;
 
         /// <summary>
         /// Searches for a <paramref name="sequence"/> within an <see cref="ReadOnlySpan{T}"/> and returns the index of the first occurrence.
         /// </summary>
-        /// <typeparam name="T">The type of elements in the array.</typeparam>
+        /// <typeparam name="T">The type of elements in the sequences.</typeparam>
         /// <param name="values">The array to search in.</param>
         /// <param name="sequence">The sequence to search for.</param>
         /// <param name="start">The starting index for the search. Default is 0.</param>
         /// <returns>The index of the first occurrence of the sequence, or -1 if not found.</returns>
         [DebuggerStepThrough]
-        public static int SequenceSearch<T>(this ReadOnlySpan<T> values, ReadOnlySpan<T> sequence)
+        public static int SequenceSearch<T>(this ReadOnlySpan<T> values, ReadOnlySpan<T> sequence) where T : IEquatable<T>
         {
             if (!sequence.IsEmpty || values.Length < sequence.Length)
                 return -1;
@@ -50,9 +50,30 @@ namespace AuroraLib.Core.Extensions
         }
 
         /// <summary>
+        /// Determines the maximum matching length between two ReadOnlySpans.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the sequences.</typeparam>
+        /// <param name="sequence1">The first ReadOnlySpan to compare.</param>
+        /// <param name="sequence2">The second ReadOnlySpan to compare.</param>
+        /// <returns>The length of the maximum matching prefix between the two ReadOnlySpans.</returns>
+        public static int MaxMatch<T>(this ReadOnlySpan<T> sequence1, ReadOnlySpan<T> sequence2) where T : IEquatable<T>
+        {
+            int minLength = Math.Min(sequence1.Length, sequence2.Length);
+            for (int i = 0; i < minLength; i++)
+            {
+                if (!sequence1[i].Equals(sequence2[i]))
+                {
+                    return i;
+                }
+            }
+
+            return minLength;
+        }
+
+        /// <summary>
         /// Counts the occurrences of a specific <paramref name="value"/> of <typeparamref name="T"/> in a <see cref="ReadOnlySpan{T}"/>.
         /// </summary>
-        /// <typeparam name="T">The type of elements in the span.</typeparam>
+        /// <typeparam name="T">The type of elements in the sequences.</typeparam>
         /// <param name="span">The read-only span to count the occurrences in.</param>
         /// <param name="value">The value to count.</param>
         /// <returns>The number of occurrences of the value in the span.</returns>
@@ -74,7 +95,7 @@ namespace AuroraLib.Core.Extensions
         /// <summary>
         /// Computes the hash code for a <see cref="ReadOnlySpan{T}"/> of elements using the <see cref="XXHash32"/> algorithm.
         /// </summary>
-        /// <typeparam name="T">The type of elements in the input span.</typeparam>
+        /// <typeparam name="T">The type of elements in the sequences.</typeparam>
         /// <param name="span">The read-only span of elements to compute the hash code from.</param>
         /// <returns>The computed hash code.</returns>
         [DebuggerStepThrough]
