a6460e8e035f9cdfe82e47b054d909cd54ccbd10
Venomalia <87765258+Venomalia@users.noreply.github.com>
2023-12-12 17:21:02 +0100

    add IObjectName

---
+++ src/AuroraLib.Core/Interfaces/IObyektName.cs
@@ -0,0 +1,13 @@
+﻿namespace AuroraLib.Core.Interfaces
+{
+    /// <summary>
+    /// Represents an interface for objects that have a name.
+    /// </summary>
+    public interface IObjectName
+    {
+        /// <summary>
+        /// Gets the name of the object.
+        /// </summary>
+        string Name { get; }
+    }
+}
