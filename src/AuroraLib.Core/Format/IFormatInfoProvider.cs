c42f2592eefecbb32284f570b22da0b00a18ff0e
Venomalia <87765258+Venomalia@users.noreply.github.com>
2025-01-16 20:20:41 +0100

    Rename IFormatProvider to IFormatInfoProvider

--- src/AuroraLib.Core/Format/IFormatInfoProvider.cs
+++ src/AuroraLib.Core/Format/IFormatInfoProvider.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace AuroraLib.Core.Format
 {
     /// <summary>
     /// Defines a provider that supplies file format information.
     /// </summary>
-    public interface IFormatProvider
+    public interface IFormatInfoProvider
     {
         /// <summary>
         /// Gets the file format information associated with the provider.
